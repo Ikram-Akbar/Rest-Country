a977e8164513068837cc48bdea96ee0f56b6989d
Ikram-Git-Coder <ikramakbar71@gmail.com>
2022-08-05 18:42:09 +0600

    fetch the data

--- src/pages/Countries/Countries.js
+++ src/pages/Countries/Countries.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Countries = () => {
-    return (
-        <div>
-            <h1>this is Countries page </h1>
-        </div>
-    );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const url = "https://restcountries.com/v3.1/all";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, [countries]);
+  return (
+    <div>
+      <h1>
+        There are <span>{countries.length}</span> in the world{" "}
+      </h1>
+    </div>
+  );
 };
 
 export default Countries;
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomLink from './CustomLink/CustomLink';
+import CustomLink from '../../CustomLink/CustomLink';
 
 const Header = () => {
     return (
